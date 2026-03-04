fdf71b1d3d699f3a0b09e0e2c80d999451afcf54
João Guilherme Braga Nascimento <guilhermebragabr2015@gmail.com>
2026-03-03 21:47:02 -0300

    Interface gráfica lendo arquivo implementado

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from reader import read_txt
+import streamlit as st # rodar o comando no terminal "streamlit run main.py" para rodar o código
 
-archive_path = input("Informe o caminho do arquivo txt: ")
+st.title("Interface gráfica da AV1 de Projeto de banco de dados")
 
-db_list = read_txt.read_file_txt(archive_path)
+st.header("Alunos: ")
+st.subheader("João Guilherme Braga Nascimento / 2210285")
+st.subheader("Levy Gomes Porfirio Brito / 2223882")
+st.subheader("Lucas Diniz Frota / 2310302")
 
-print(db_list)
+st.divider()
+
+st.header("Informe o arquivo a ser lido: ")
+
+archive = st.file_uploader("Escolha um arquivo", type=["txt", "csv"])
+
+if archive is not None:
+    st.success("Arquivo enviado com sucesso!")
+
+    content = archive.read().decode("utf-8").splitlines()
+    st.text(content)
--- reader/read_txt.py
+++
@@ -1,3 +0,0 @@
-def read_file_txt(path: str) -> list:
-    with open(path, 'r') as archive:
-        return archive.read().splitlines()
